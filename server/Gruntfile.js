4536303500e4a97cf686abd157c6eca9d7f8881f
Alex Nitta <a.j.nitta@gmail.com>
2016-05-26 16:41:05 -0700

    (fix) Update gruntfile to use https in ngconstant

--- server/Gruntfile.js
+++ server/Gruntfile.js
@@ -104,7 +104,7 @@ module.exports = function(grunt) {
         constants: {
           ENV: {
             name: 'development',
-            apiEndpoint: 'http://localhost:443'
+            apiEndpoint: 'https://localhost:443'
           }
         }
       },
@@ -115,7 +115,7 @@ module.exports = function(grunt) {
         constants: {
           ENV: {
             name: 'production',
-            apiEndpoint: 'http://192.241.235.109:443'
+            apiEndpoint: 'https://192.241.235.109:443'
           }
         }
       }
